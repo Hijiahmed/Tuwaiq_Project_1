cea778befcbe8452bedf4ca8a85a2c93579227df
Hijiahmed <hiji.ahmed.rae.js@tuwaiq.edu.sa>
2021-10-17 15:10:10 +0300

    start project

---
+++ project.js
@@ -0,0 +1,18 @@
+let search = $(".search");
+let h1 = $("h1");
+let meneu = $("meneu");
+let UserName = $("UserName");
+let pass = $("pass");
+let clubs = $("clubs");
+let buy = $("buy");
+let size = $("size");
+
+let team = [
+  {
+    teamName: "itihad",
+    price: 20.0,
+  },
+];
+for (let i = 0; i < team.length; i++) {
+  return team;
+}
